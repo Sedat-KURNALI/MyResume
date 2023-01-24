1feefb40113c55ba6e2753c195d0fc339f2ef272
Sedat-KURNALI <sedatkurnali@gmail.com>
2023-01-24 11:48:29 +0300

    first commit

---
+++ src/components/header/copyright/copyright.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./copyright.css";
+
+const Copyright = () => {
+  return (
+    <div className="copyright">
+      <span>© Copyright 2023 Krnl_Sdt</span>
+    </div>
+  );
+};
+
+export default Copyright;
